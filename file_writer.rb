b1812036ad314a14fae257bc7783b38bef19fc23
Feather <lisafeather@gmail.com>
2014-04-25 13:22:17 -0700

    its aliiiiive! yay! no more long & boring web page updates

--- file_writer.rb
+++ file_writer.rb
@@ -1,6 +1,24 @@
-            <li>
-              <figure>
-                <img src="img-silkscreen/thumb/embarcadero_thumb.jpg" alt="embarcadero"/>
-                <figcaption><h4>Embarcadero (2012)</h4></figcaption>
-              </figure>
-            </li>
+puts "file name"
+file_name = gets.chomp 
+
+images = File.readlines(file_name)
+newFile = File.open("newAcrylic.html", "a")
+title = ""
+
+images.each do |i| 
+  title = i.chomp("_thumb.jpg\n")
+  puts title
+  newFile.write("<li>\n")
+  newFile.write("\t<figure>\n")
+  newFile.write("\t\t<img src='img-acrylic/thumb/" + i.chomp + "' alt='"+ i.chomp + "'/>\n")
+  newFile.write("\t\t\t<figcaption><h4>" + title.capitalize + " (2012)</h4></figcaption>\n")
+  newFile.write("\t</figure>\n")
+  newFile.write("</li>\n")
+end
+
+            # <li>
+            #   <figure>
+            #     <img src="img-silkscreen/thumb/embarcadero_thumb.jpg" alt="embarcadero"/>
+            #     <figcaption><h4>Embarcadero (2012)</h4></figcaption>
+            #   </figure>
+            # </li>
