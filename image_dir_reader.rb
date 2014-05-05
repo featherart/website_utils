bbb1e3c6c6d9c8f533c0708c543d4dae8a2d3dae
Feather <lisafeather@gmail.com>
2014-05-04 19:01:17 -0700

    update to add new style to filewriter and directories to file reader

--- image_dir_reader.rb
+++ image_dir_reader.rb
@@ -7,6 +7,10 @@ o_big_path = "../new_website/img-oil/large"
 o_little_path = "../new_website/img-oil/thumb"
 l_big_path = "../new_website/img-linen/large"
 l_little_path = "../new_website/img-linen/thumb"
+ab_little_path = "../new_website/img-abstract/thumb"
+ab_big_path = "../new_website/img-abstract/large"
+f_little_path = "../new_website/img-floral/thumb"
+f_big_path = "../new_website/img-floral/large"
 
 #write to each file to list the images
 a_little_f = File.open("acrylic_small.txt", "a")
@@ -33,3 +37,14 @@ Dir.foreach(l_little_path) {|x| l_little_f.write("#{x}\n") }
 l_big_f = File.open("linen_big.txt", "a")
 Dir.foreach(l_big_path) {|x| l_big_f.write("#{x}\n") }
 
+ab_big_f = File.open("abstract_big.txt", "a")
+Dir.foreach(ab_big_path) {|x| ab_big_f.write("#{x}\n")}
+
+ab_little_f = File.open("abstract_little.txt", "a")
+Dir.foreach(ab_little_path) {|x| ab_little_f.write("#{x}\n")}
+
+f_big_f = File.open("floral_big.txt", "a")
+Dir.foreach(f_little_path) {|x| f_big_f.write("#{x}\n")}
+
+f_little_f = File.open("floral_little.txt", "a")
+Dir.foreach(f_little_path) {|x| f_little_f.write("#{x}\n")}
--- new_file_writer.rb
+++ new_file_writer.rb
@@ -3,20 +3,23 @@ file_name = gets.chomp
 
 images = File.readlines("img_listings/#{file_name}")
 # rename file here! also don't forget to change img- src below
-newFile = File.open("new_html/little_acrylic.html", "a")
+newFile = File.open("new_html/abstract.html", "a")
 title = ""
 
 images.each do |i| 
   title = i.chomp("_thumb.jpg\n")
   puts title
-  newFile.write("\t\t\t\t<div class='item w2'>\n")
-  newFile.write("\t\t\t\t\t<img src='img-acrylic/thumb/" + i.chomp + "' alt='"+ i.chomp + "'/>\n")
+  newFile.write("\t\t\t\t<div class='item'>\n")
+  newFile.write("\t\t\t\t\t<a href='img-abstract/large/'"+i.chomp + " data-lightbox='image-1' data-title='" + title.capitalize + " (2012), oil on canvas, 12\"x12\"'>\n")
+  newFile.write("\t\t\t\t\t<img src='img-abstract/thumb/" + i.chomp + "' alt='"+ i.chomp + "'/>\n")
   newFile.write("\t\t\t\t\t<figcaption><h4>" + title.capitalize + "</h4></figcaption>\n")
   newFile.write("\t\t\t\t</div>\n")
 end
 
 # should generate this:
-# <div class="item w2">
-#<img src='img-acrylic/thumb/geisha_thumb.jpg' alt='geisha_thumb.jpg'/>
-# <figcaption><h4>Geisha </h4></figcaption>
+# <div class='item'>
+#   <a href="img-acrylic/large/rainyday_web.jpg" data-lightbox="image-1" data-title="Full Moon">
+#   <img src='img-acrylic/thumb/rainyday_thumb.jpg' alt='rainyday_thumb.jpg'/>
+#   <figcaption><h4>Rainyday</h4></figcaption>
+#   </a>
 # </div>
